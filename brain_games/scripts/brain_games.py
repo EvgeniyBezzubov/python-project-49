f4e2ddd1ba7f10c2511eca622adbcadb228b1cb3
Evgeniy.Bezzubov <raindrops228@gmail.com>
2023-10-05 13:19:54 +1000

    aaaaaaa

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,11 +3,11 @@ import prompt
 import random
 
 
-def main():
+def base():
     print("Welcome to the Brain Games!")
     name = prompt.string('May I have your name? ')
     print("Hello, {0}!".format(name))
 
 
 if __name__ == "__main__":
-    main()
+    base()
