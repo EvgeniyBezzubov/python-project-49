ed4595ae6010a08005f8b29f708eb8d6a76f526b
Evgeniy.Bezzubov <raindrops228@gmail.com>
2023-10-29 02:11:36 +1000

    add const, and other

--- brain_games/drive.py
+++ brain_games/drive.py
@@ -3,17 +3,21 @@ import prompt
 
 def ans_validator(name, get_answer_and_question):
     name = name()
-    NUM_CORRECT_ANS = 0  # Перемещаем запрос имени пользователя в начало функции
+    NUM_CORRECT_ANS = 0  # Перемещаем запрос имени \
+    # пользователя в начало функции
     while NUM_CORRECT_ANS < 3:
-        right_ans = get_answer_and_question()  # Вызываем функцию для получения вопроса и ответа
+        right_ans = get_answer_and_question()
+            # Вызываем функцию для получения вопроса и ответа
         ans = prompt.string('Your answer: ')
         if str(right_ans) == str(ans):
             print("Correct!")
             NUM_CORRECT_ANS += 1
         else:
-            print(f"'{ans}' is wrong answer ;(. Correct answer was '{right_ans}'.")
+            print(f"'{ans}' is wrong answer ;(.\
+             Correct answer was '{right_ans}'.")
             print("Let's try again, {0}!".format(name))
-            break  # Завершаем выполнение функции после неправильного ответа
+            break  # Завершаем выполнение функции\
+            # после неправильного ответа
 
         if NUM_CORRECT_ANS == 3:
             print("Congratulations, {0}!".format(name))
