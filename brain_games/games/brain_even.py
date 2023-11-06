33e79a95b6796a4528b48330a3cb31155ae51440
Evgeniy.Bezzubov <raindrops228@gmail.com>
2023-11-07 01:02:27 +1000

    ss

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -5,18 +5,18 @@ from brain_games.drive import user_response_request
 
 def return_r_ans_and_question_even():
     generated_random_num = random.randint(1, 100)
-    question = 'Question:'+ str(generated_random_num)
-    correct_answer = get_correct_answer(generated_random_num)
+    question = 'Question:' + str(generated_random_num)
+    correct_answer = is_correct_answer(generated_random_num)
     return [correct_answer, question]
 
 
-def start_game_even():
-    user_response_request(EVEN_INSTRUCTION, return_r_ans_and_question_even)
-
-
-def get_correct_answer(generated_random_num):
+def is_correct_answer(generated_random_num):
     if generated_random_num % 2 == 1:
         correct_answer = "no"
     else:
         correct_answer = "yes"
     return correct_answer
+
+
+def start_game_even():
+    user_response_request(EVEN_INSTRUCTION, return_r_ans_and_question_even)
