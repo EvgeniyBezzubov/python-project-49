70e130f7fa6f119e4c64d7b3c6b06cdc7b8735a4
Evgeniy.Bezzubov <raindrops228@gmail.com>
2023-11-19 01:20:49 +1000

    zero

--- brain_games/const.py
+++ brain_games/const.py
@@ -1,11 +1,11 @@
-even_instruction = 'Answer "yes" if the number is even, \
+EVEN_INSTRUCTION = 'Answer "yes" if the number is even, \
 otherwise answer "no".'
-calc_instruction = "What is the result of the expression?"
-progression_instruction = "What number is missing in\
+CALC_INSTRUCTION = "What is the result of the expression?"
+PROGRESSION_INSTRUCTION = "What number is missing in\
  the progression?"
-prime_instruction = 'Answer "yes" if given number is prime.\
+PRIME_INSTRUCTION = 'Answer "yes" if given number is prime.\
  Otherwise answer "no".'
-gcd_instruction = "Find the greatest common divisor\
+GCD_INSTRUCTION = "Find the greatest common divisor\
  of given numbers."
 NUM_OF_ROUNDS = 3
 LST_OPERATIONS = ["+", "-", "*"]
