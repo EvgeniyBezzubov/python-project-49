0a19490e93e0f2821c514b5be8e2352af5d2df80
Evgeniy.Bezzubov <raindrops228@gmail.com>
2023-11-24 00:24:37 +1000

    renaming

--- brain_games/utils.py
+++ brain_games/utils.py
@@ -2,10 +2,14 @@ import random
 
 
 def rand_int_1_100(a=1, b=100):
-    return random.randint(a, b)
+    num = random.randint(a, b)
+    return num
 def rand_int_1_20(a=1, b=20):
-    return random.randint(a, b)
+    num = random.randint(a, b)
+    return num
 def rand_int_1_5(a=1, b=5):
-    return random.randint(a, b)
+    num = random.randint(a, b)
+    return num
 def rand_int(a, b):
-    return random.randint(a, b)
+    num = random.randint(a, b)
+    return num
