050b7c4281b5882ff8b172525c694750950f8639
Evgeniy.Bezzubov <raindrops228@gmail.com>
2023-11-27 23:49:14 +1000

    rand int

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -7,19 +7,15 @@ from brain_games.utils import rand_int_1_100
 
 def generate_r_ans_and_question_for_calc():
     generated_random_num_1, generated_random_num_2 = rand_int_1_100(), rand_int_1_100()
-    rand_operation = LST_OPERATIONS[random.randint(0,3)]
+    rand_operation = LST_OPERATIONS[random.randint(0, 3)]
     question = f'Question: {generated_random_num_1} ' \
                f'{rand_operation} {generated_random_num_2}'
-    right_ans = str(get_nums_and_return_right_ans(
+    right_ans = str(put_in_nums_and_operation_then_return_right_ans(
         generated_random_num_1, rand_operation, generated_random_num_2))
     return right_ans, question
 
 
-def start_game_calc():
-    start_game(CALC_INSTRUCTION, generate_r_ans_and_question_for_calc)
-
-
-def get_nums_and_return_right_ans(
+def put_in_nums_and_operation_then_return_right_ans(
         num_1,
         operation,
         num_2):
@@ -30,3 +26,7 @@ def get_nums_and_return_right_ans(
     elif operation == "*":
         ans = num_1 * num_2
     return ans
+
+
+def start_game_calc():
+    start_game(CALC_INSTRUCTION, generate_r_ans_and_question_for_calc)
